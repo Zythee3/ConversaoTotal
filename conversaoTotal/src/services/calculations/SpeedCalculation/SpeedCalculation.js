a6c4d7c611c82acca86fc41a682568b305a33da5
matheus guilherme <zmatheusguilherme2@gmail.com>
2025-02-18 21:32:04 -0300

    atualização

--- conversaoTotal/src/services/calculations/SpeedCalculation/SpeedCalculation.js
+++ conversaoTotal/src/services/calculations/SpeedCalculation/SpeedCalculation.js
@@ -5,9 +5,16 @@ const SpeedConversion = {
 
 
 const SpeedCalculationResult = (value, unit, unit2) => {
-
-//   if (unit == "")
-  const result =  value * SpeedConversion[unit][unit2];
+  let result = 0;
+  if (unit == "Metros por segundos (m/s)" && unit2 == "Quilômetros por hora (km/h)"){
+    result =  value * SpeedConversion[unit][unit2];
+  }
+  else if (unit == "Quilômetros por hora (km/h)" && unit2 == "Metros por segundos (m/s)"){
+    result =  value / SpeedConversion[unit][unit2];
+  }
+  else{
+    result = value;
+  }
   return result;
 };
 
