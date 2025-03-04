885b1ae212cd8bcad0894fa6d47ab5458a07ae6d
matheus guilherme <zmatheusguilherme2@gmail.com>
2025-03-03 21:27:24 -0300

    ajustes

--- conversaoTotal/src/components/Header/Header.jsx
+++ conversaoTotal/src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./Header.css";
 import OptionsHeader from "../OptionsHeader/OptionsHeader";
-import IconSite from "../../assets/icons/IconSite.svg"
+import IconSite from "../../assets/icons/IconSite.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="divNavBar">
+      <Link to="/">
       <div className="tituloLogo">
         <div className="siteLogo">
           <img src={IconSite} alt="" />
         </div>
         <span>Conversão Total</span>
       </div>
+      </Link>
       <div className="OptionsUnitHeader">
         <OptionsHeader />
       </div>
